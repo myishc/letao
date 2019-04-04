e0e4fd7d6603b362cd7ef819001c76209936dc91
myishc <844271143@qq.com>
2019-04-04 23:38:59 +0800

    乐淘第一次上传index.html,category.html 2019/4/4 23:37

---
+++ public/m/js/category.js
@@ -0,0 +1,12 @@
+
+
+// 列表滑动
+
+mui('#commList.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005,//flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    indicators: false 
+});
+mui('#commShow.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005,//flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    indicators: true 
+});
---
+++ public/m/js/index.js
@@ -0,0 +1,13 @@
+
+//自动轮播图
+    mui("#slider").slider({
+        interval: 2000
+    });
+        
+    // 滑动
+    mui('#main.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        indicators: false 
+    });
+				
+			
